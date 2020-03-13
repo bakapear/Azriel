413d0bff8462068156ca127f7f3758b886511c8c
bakapear <proxybomb@gmail.com>
2020-03-13 23:26:54 +0100

    added translate cmd

---
+++ src/cmds/translate.js
@@ -0,0 +1,45 @@
+let dp = require('despair')
+
+module.exports = {
+  name: 'translate',
+  aliases: ['trans'],
+  description: `Translates text to english.`,
+  permissions: [],
+  args: 1,
+  usage: '<text>',
+  exec: async (msg, cmd) => {
+    let res = await translateGoogle(cmd.content)
+    msg.channel.send({
+      embed: {
+        description: res.text,
+        footer: {
+          text: `${res.lang}-en | ${res.acc}% Accuracy`
+        }
+      }
+    })
+  }
+}
+
+async function translateGoogle (text, target = 'en') {
+  let body = await dp('https://translate.googleapis.com/translate_a/single', {
+    query: {
+      client: 'gtx',
+      sl: 'auto',
+      tl: target,
+      hl: target,
+      dt: 't',
+      ie: 'UTF-8',
+      oe: 'UTF-8',
+      otf: 1,
+      ssel: 0,
+      tsel: 0,
+      kc: 7,
+      q: text
+    }
+  }).json()
+  return {
+    text: body[0].map(x => x[0]).join('').replace(/>/g, `\\>`),
+    acc: (body[6] * 100).toFixed(2),
+    lang: body[8][0][0]
+  }
+}
