51e2177b286d9630fe20e46aec92bbff0e26b4b6
bakapear <proxybomb@gmail.com>
2020-03-15 19:23:09 +0100

    added radio cmd

---
+++ src/cmds/radio.js
@@ -0,0 +1,37 @@
+let util = require('../util')
+let dp = require('despair')
+
+module.exports = {
+  name: 'radio',
+  aliases: [],
+  description: `Gets a list of radio stations.`,
+  permissions: [],
+  args: 1,
+  usage: '<query> (offset)',
+  exec: async (msg, cmd) => {
+    let res = await util.poker(getStations, cmd, true)
+    if (!res.items.length || res.offset < 0) return msg.channel.send('Nothing found!')
+    let total = res.items.length
+    res.items.splice(0, res.offset)
+    if (res.items.length > 15) res.items.length = 15
+    msg.channel.send({
+      embed: {
+        title: 'Radio Stations',
+        description: res.items.map((x, i) => `${i + 1}. [${x.name.trim()}](${x.url_resolved.trim()})`).join('\n'),
+        footer: {
+          text: `Showing ${res.items.length} stations (${res.offset} - ${total})`
+        }
+      }
+    })
+  }
+}
+
+async function getStations (query) {
+  let body = await dp('https://de1.api.radio-browser.info/json/stations/search', {
+    query: {
+      name: query,
+      limit: 250
+    }
+  }).json()
+  return body
+}
--- src/util/index.js
+++ src/util/index.js
@@ -1,19 +1,20 @@
 let main = {
   rnd: x => Math.floor(Math.random() * x.length),
-  poker: async (fn, cmd) => {
+  poker: async (fn, cmd, seperate) => {
     let last = cmd.args[cmd.args.length - 1]
     if (!cmd.args.length) {
       let res = await fn(cmd.content)
-      return res[main.rnd(res)]
+
+      return seperate ? { items: res, offset: main.rnd(res) } : res[main.rnd(res)]
     } else if (cmd.args.length > 1 && (!isNaN(last) || last === '?')) {
       let res = await fn(cmd.args.slice(0, cmd.args.length - 1).join(' '))
       let offset = main.rnd(res)
       if (!isNaN(last)) offset = parseInt(last) - 1
       if (offset >= res.length) offset = res.length - 1
-      return res[offset]
+      return seperate ? { items: res, offset } : res[offset]
     } else {
       let res = await fn(cmd.content)
-      return res[0]
+      return seperate ? { items: res, offset: 0 } : res[0]
     }
   },
   attachImages: require('./attachImages.js')
