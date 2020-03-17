447b9e353560036101f692b3bb45c1cb768cf701
bakapear <proxybomb@gmail.com>
2020-03-17 20:12:37 +0100

    SMASH

--- src/gadgets/presence.js
+++ src/gadgets/presence.js
@@ -8,5 +8,5 @@ let types = ['PLAYING', 'STREAMING', 'LISTENING', 'WATCHING']
 
 setInterval(async () => {
   if (!words.length) words = (await dp('https://api.urbandictionary.com/v0/random').json()).list.map(x => x.word)
-  bot.user.setPresence({ activity: { name: words.shift().substring(0, 25), type: util.rnd(types), url: 'https://twitch.tv/bakapear' } })
+  bot.user.setPresence({ activity: { name: words.shift().substring(0, 25), type: util.rnd(types), url: 'https://twitch.tv/btssmash' } })
 }, 654321)
