01fa45d131398a7c78afeb54ba1993fc73000bae
bakapear <proxybomb@gmail.com>
2020-03-19 20:32:50 +0100

    random util fix

--- src/util/random.js
+++ src/util/random.js
@@ -1,7 +1,11 @@
 let main = {
   randomFloat: (min, max) => Math.random() * (max - min) + min,
-  randomInt: (min, max) => Math.floor(main.randomFloat(min, max)),
-  randomIndex: arr => main.randomInt(0, arr.length),
+  randomInt: (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  },
+  randomIndex: arr => main.randomInt(0, arr.length - 1),
   randomItem: arr => arr[main.randomIndex(arr)]
 }
 
