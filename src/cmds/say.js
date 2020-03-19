f8a390538c25503894469da53be37b894c47c6af
bakapear <proxybomb@gmail.com>
2020-03-19 16:52:53 +0100

    added say cmd

---
+++ src/cmds/say.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'say',
+  aliases: [],
+  description: 'Wait, I didn\'t say that... Or did I?',
+  permissions: [],
+  args: 1,
+  usage: '<message>',
+  exec: async (msg, cmd) => {
+    msg.channel.send(cmd.content)
+  }
+}
