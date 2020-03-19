32b73429e54820118595d7cd5803170128c150df
bakapear <proxybomb@gmail.com>
2020-03-19 20:36:00 +0100

    added roll cmd

---
+++ src/cmds/roll.js
@@ -0,0 +1,20 @@
+let util = require('../util')
+
+module.exports = {
+  name: 'roll',
+  aliases: ['dice'],
+  description: 'Rolls a dice.',
+  permissions: [],
+  args: 0,
+  usage: '(max) | <min> <max>',
+  exec: async (msg, cmd) => {
+    let min = 1
+    let max = 6
+    if (!isNaN(cmd.args[0])) max = parseInt(cmd.args[0])
+    if (!isNaN(cmd.args[1])) {
+      min = max
+      max = parseInt(cmd.args[1])
+    }
+    msg.channel.send(`Rolled a **${util.randomInt(min, max)}**!`)
+  }
+}
