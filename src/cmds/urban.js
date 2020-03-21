20cdd92da955b085c10deb20698f1218ebf3cc0a
bakapear <proxybomb@gmail.com>
2020-03-21 14:13:33 +0100

    added urban cmd

---
+++ src/cmds/urban.js
@@ -0,0 +1,30 @@
+let util = require('../util')
+let dp = require('despair')
+
+module.exports = {
+  name: 'urban',
+  aliases: ['define'],
+  description: 'Gets an urban dictionary definition of a word.',
+  permissions: [],
+  args: 1,
+  usage: '<query> (poker)',
+  exec: async (msg, cmd) => {
+    let res = await util.poker(getDefinition, cmd)
+    if (!res.items.length) return msg.channel.send('Nothing found!')
+    let item = res.item
+    return util.showEmbed(msg.channel, {
+      title: item.word,
+      url: item.permalink,
+      description: [item.definition, item.example].join('\n\n'),
+      timestamp: item.written_on,
+      footer: { text: `by ${item.author}` }
+    })
+  }
+}
+
+async function getDefinition (term) {
+  let body = await dp('http://api.urbandictionary.com/v0/define', {
+    query: { term }
+  }).json()
+  return body.list
+}
