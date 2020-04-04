36c7069871343a27c6500d5d476c324916226b84
bakapear <proxybomb@gmail.com>
2020-04-04 09:48:19 +0200

    added gelbooru command

---
+++ src/cmds/gelbooru.js
@@ -0,0 +1,36 @@
+let util = require('../util')
+let dp = require('despair')
+
+module.exports = {
+  name: 'gelbooru',
+  aliases: ['gel'],
+  description: 'Gets a naughty post from Gelbooru.',
+  permissions: [],
+  args: 0,
+  usage: '(tags) (poker)',
+  exec: async (msg, cmd) => {
+    let res = await util.poker(getPosts, cmd)
+    if (!res.items.length) return msg.channel.send('Nothing found!')
+    let item = res.item
+    let img = await util.attachImages([item.file_url])
+    return msg.channel.send({ files: img })
+  }
+}
+
+async function getPosts (query) {
+  let res = await dp('https://gelbooru.com/index.php', {
+    query: {
+      tags: query,
+      page: 'dapi',
+      s: 'post',
+      q: 'index',
+      limit: 250,
+      json: 1
+    }
+  }).json()
+  return res.filter(x =>
+    x.tags.indexOf('scat') < 0 &&
+    x.tags.indexOf('guro') < 0 &&
+    x.tags.indexOf('furry') < 0 &&
+    x.tags.indexOf('astolfo_(fate)') < 0)
+}
