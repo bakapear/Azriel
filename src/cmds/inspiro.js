413af1d26b91f4f4951cc6e797d260411d0df461
bakapear <proxybomb@gmail.com>
2020-04-22 09:04:47 +0200

    added inspiro cmd

---
+++ src/cmds/inspiro.js
@@ -0,0 +1,16 @@
+let util = require('../util')
+let dp = require('despair')
+
+module.exports = {
+  name: 'inspiro',
+  aliases: [],
+  description: 'Displays a random motivational picture.',
+  permissions: [],
+  args: 0,
+  usage: '',
+  exec: async (msg, cmd) => {
+    let { body } = await dp('http://inspirobot.me/api', { query: { generate: true } })
+    let img = await util.attachImages([body])
+    return msg.channel.send({ files: img })
+  }
+}
