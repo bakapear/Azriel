ed47f400a1362fd74bf7182ccb3015a0116e0a16
bakapear <proxybomb@gmail.com>
2020-06-07 18:47:11 +0200

    marisa now knows whats up

--- src/cmds/marisa.js
+++ src/cmds/marisa.js
@@ -1,3 +1,4 @@
+let dp = require('despair')
 let util = require('../util')
 
 module.exports = {
@@ -8,7 +9,25 @@ module.exports = {
   args: 0,
   usage: '',
   exec: async (msg, cmd) => {
-    let img = await util.attachImages(['https://i.imgur.com/aVf2qHc.png'])
+    let posts = await getPosts('kirisame_marisa rating:safe 1girl')
+    let item = util.randomItem(posts)
+    let img = await util.attachImages([item.file_url])
     return msg.channel.send({ files: img })
   }
+}
+
+async function getPosts (query) {
+  try {
+    let res = await dp('https://gelbooru.com/index.php', {
+      query: {
+        tags: query,
+        page: 'dapi',
+        s: 'post',
+        q: 'index',
+        limit: 500,
+        json: 1
+      }
+    }).json()
+    return res
+  } catch (e) { return [] }
 }
