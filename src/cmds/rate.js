64420c1d43d52fc68de9f321657bfd85d83cb882
bakapear <proxybomb@gmail.com>
2020-06-07 18:11:46 +0200

    added rate cmd

---
+++ src/cmds/rate.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'rate',
+  aliases: [],
+  description: 'Rates something.',
+  permissions: [],
+  args: 0,
+  usage: '',
+  exec: async (msg, cmd) => {
+    let query = cmd.content.toLowerCase()
+    if (['jibril', 'azriel'].includes(query)) return msg.channel.send('Top tier.')
+    let rnd = Math.floor(random(query) * 11)
+    let rate = `Rated **${rnd}/10**!`
+    if (rnd === 10) rate += ' <3'
+    return msg.channel.send(rate)
+  }
+}
+
+function random (word) {
+  if (!word) return Math.random()
+  let seed = word.split('').map(x => x.charCodeAt(0)).join('')
+  let x = Math.sin(seed++) * 10000
+  return x - Math.floor(x)
+}
