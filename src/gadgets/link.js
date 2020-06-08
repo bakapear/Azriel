a71fb33ec9b690f46ef8f660489b2b04b0fb8789
bakapear <proxybomb@gmail.com>
2020-06-08 21:30:48 +0200

    monopoly

--- src/gadgets/link.js
+++ src/gadgets/link.js
@@ -1,4 +1,5 @@
 /* global bot */
+let util = require('../util')
 
 module.exports = msg => {
   if (msg.content.startsWith('https://discordapp.com/channels')) {
@@ -7,18 +8,16 @@ module.exports = msg => {
     if (parts.length === 3) {
       let channel = bot.channels.cache.find(x => x.id === parts[1])
       channel.messages.fetch(parts[2]).then(m => {
-        msg.channel.send({
-          embed: {
-            description: m.content,
-            image: {
-              url: m.attachments.first() ? m.attachments.first().url : null
-            },
-            footer: {
-              text: m.author.username,
-              icon_url: m.author.avatarURL()
-            },
-            timestamp: m.createdAt
-          }
+        util.showEmbed(msg.channel, {
+          description: m.content,
+          image: {
+            url: m.attachments.first() ? m.attachments.first().url : null
+          },
+          footer: {
+            text: m.author.username,
+            icon_url: m.author.avatarURL()
+          },
+          timestamp: m.createdAt
         })
       })
       return true
