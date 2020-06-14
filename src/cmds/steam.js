214aacbce738521e4240e47f165309910ec17cef
bakapear <proxybomb@gmail.com>
2020-06-14 23:26:14 +0200

    steam fixxu

--- src/cmds/steam.js
+++ src/cmds/steam.js
@@ -14,7 +14,7 @@ module.exports = {
     if (!user) return msg.channel.send('User not found!')
     return util.showEmbed(msg.channel, {
       title: user.name,
-      description: `:flag_${user.flag.toLowerCase()}: (${user.level}) ${user.created.substr(0, user.created.indexOf('T'))}`,
+      description: `${user.flag ? `:flag_${user.flag.toLowerCase()}: ` : ''}(${user.level}) ${user.created.substr(0, user.created.indexOf('T'))}`,
       fields: [
         {
           name: '\u200b',
@@ -43,13 +43,16 @@ async function getPlayerData (user) {
   }).catch(e => { return { body: null } })
   if (!body) return null
   let $ = hy(body)
-  return {
+  let res = {
     name: util.decodeEntities($('.header-title>a').text()),
     url: $('.header-title>a')[0].attribs.href,
     avatar: $('.avatar')[0].attribs.src,
     level: Number($('.friendPlayerLevel').text()),
-    flag: $('.flag')[0].attribs.alt,
+    flag: $('.flag')[0],
     created: $('.player-info .number')[0].attribs.title,
     stats: $('table')[0].children[1].children.filter(x => x.type === 'element').map(x => [$(x.children[1]).text(), $(x.children[3]).text()]).slice(0, -1)
   }
+  if (res.flag) res.flag = res.flag.attribs.alt
+  if (res.avatar.endsWith('appicon.svg')) res.avatar = 'https://i.imgur.com/ltK44WQ.jpg'
+  return res
 }
