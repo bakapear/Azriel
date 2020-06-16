526c7def119e53efa255d28f816c4e3d27c413aa
bakapear <proxybomb@gmail.com>
2020-06-16 10:38:09 +0200

    fixed link

--- src/cmds/steam.js
+++ src/cmds/steam.js
@@ -14,19 +14,10 @@ module.exports = {
     if (!user) return msg.channel.send('User not found!')
     return util.showEmbed(msg.channel, {
       title: user.name,
-      description: `${user.flag ? `:flag_${user.flag.toLowerCase()}: ` : ''}(${user.level}) ${user.created.substr(0, user.created.indexOf('T'))}`,
-      fields: [
-        {
-          name: '\u200b',
-          value: user.stats.map(x => `**${x[0]}**`),
-          inline: true
-        },
-        {
-          name: '\u200b',
-          value: user.stats.map(x => `${x[1]}`),
-          inline: true
-        }
-      ],
+      description: [
+        `${user.flag ? `:flag_${user.flag.toLowerCase()}: ` : ''}(${user.level}) ${user.created.substr(0, user.created.indexOf('T'))}\n`,
+        ...user.stats.map(x => `**${x[0]}**   ${x[1]}`)
+      ].join('\n'),
       url: user.url,
       thumbnail: { url: user.avatar }
     })
@@ -53,6 +44,6 @@ async function getPlayerData (user) {
     stats: $('table')[0].children[1].children.filter(x => x.type === 'element').map(x => [$(x.children[1]).text(), $(x.children[3]).text()]).slice(0, -1)
   }
   if (res.flag) res.flag = res.flag.attribs.alt
-  if (res.avatar.endsWith('appicon.svg')) res.avatar = 'https://i.imgur.com/ltK44WQ.jpg'
+  if (res.avatar.endsWith('appicon.svg')) res.avatar = 'https://uh.s-ul.eu/hxOaPbQB.jpg'
   return res
 }
