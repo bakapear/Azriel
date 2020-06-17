2fbf8a1ed2b7c15c4860a4f3df8a144a58d0856c
bakapear <proxybomb@gmail.com>
2020-06-17 13:40:22 +0200

    gel request entity too large fix

--- src/cmds/gelbooru.js
+++ src/cmds/gelbooru.js
@@ -19,6 +19,11 @@ module.exports = {
     if (!res.items.length) return msg.channel.send('Nothing found!')
     let item = res.item
     let webm = item.image.endsWith('.webm')
+    if (!webm && !await head(item.file_url)) {
+      item.file_url = item.file_url.replace('images/', 'samples/').replace('.png', '.jpg')
+      let index = item.file_url.lastIndexOf('/')
+      item.file_url = item.file_url.substr(0, index + 1) + 'sample_' + item.file_url.substr(index + 1)
+    }
     if (res.isList) {
       if (webm) msg.channel.send(item.file_url)
       return util.showEmbed(msg.channel, {
@@ -26,7 +31,7 @@ module.exports = {
         url: 'https://gelbooru.com/index.php?page=post&s=view&id=' + item.id,
         image: !webm ? { url: item.file_url } : null,
         timestamp: item.created_at,
-        footer: { text: item.rating + ' | ' + item.tags }
+        footer: { text: item.rating + ' | ' + util.decodeEntities(item.tags) }
       })
     }
     if (webm) {
@@ -56,4 +61,13 @@ async function getPosts (query) {
       x.tags.indexOf('furry') < 0 &&
       x.tags.indexOf('astolfo_(fate)') < 0)
   } catch (e) { return [] }
+}
+
+async function head (url) {
+  let res = await dp.head(url).catch(e => e)
+  if ((res.statusCode || res.code) <= 200) {
+    let size = Number(res.headers['content-length'] || '0')
+    let type = res.headers['content-type'] || ''
+    return size <= 8000000 && size > 0 && type.indexOf('image') >= 0 && type.indexOf('svg') < 0
+  }
 }
