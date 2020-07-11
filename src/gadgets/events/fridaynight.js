a6efd9c13ae4b5f5bc6f14eb4835fe82b2e35e14
bakapear <proxybomb@gmail.com>
2020-07-11 11:58:32 +0200

    support for the shinsengumi

--- src/gadgets/events/fridaynight.js
+++ src/gadgets/events/fridaynight.js
@@ -1,6 +1,9 @@
 /* global bot */
 
-let channel = bot.channels.cache.get('402461425743822879')
+let channels = [
+  '402461425743822879', // propa
+  '714916215713169450' // washi
+]
 let days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
 function getTimeToDay (day, h = 0, m = 0, s = 0, ms = 0) {
@@ -18,11 +21,14 @@ function getTimeToDay (day, h = 0, m = 0, s = 0, ms = 0) {
   return time
 }
 
+setTimer()
+
 function setTimer () {
   setTimeout(() => {
-    channel.send({ files: [{ attachment: 'https://uh.s-ul.eu/ACudWOPh.mp4', name: 'friday-night.mp4' }] })
+    for (let i = 0; i < channels.length; i++) {
+      let channel = bot.channels.cache.get(channels[i])
+      if (channel) channel.send({ files: [{ attachment: 'https://uh.s-ul.eu/ACudWOPh.mp4', name: 'friday-night.mp4' }] })
+    }
     setTimer()
   }, getTimeToDay('Friday', 21))
 }
-
-if (channel) setTimer()
