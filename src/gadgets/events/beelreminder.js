920217c65ab986ffcaab86c8efd797d5cd140b3b
bakapear <proxybomb@gmail.com>
2020-12-10 14:49:56 +0100

    dynamic kpop

--- src/gadgets/events/beelreminder.js
+++ src/gadgets/events/beelreminder.js
@@ -1,4 +1,5 @@
 /* global bot */
+let dp = require('despair')
 
 let channel = bot.channels.cache.get('402461425743822879')
 
@@ -16,15 +17,39 @@ function getToTime (h = 0, m = 0, s = 0, ms = 0) {
 }
 
 function setTimer () {
-  setTimeout(() => {
+  setTimeout(async () => {
     if (channel) {
-      channel.send({
-        content: '<@284425943034888204>',
-        files: [{ attachment: 'https://uh.s-ul.eu/3lArisA7.mp4', name: 'beel-health-check.mp4' }]
-      })
+      try {
+        let url = await getKPOP()
+        channel.send({
+          content: '<@284425943034888204>, ' + (url[0] || 'woof')
+        })
+      } catch (e) { console.error(e) }
     }
     setTimer()
-  }, getToTime(14))
+  }, getToTime(13))
 }
 
 if (channel) setTimer()
+
+async function getKPOP (limit = 25) {
+  let body = await dp('https://www.reddit.com/r/kpopfap/new.json', { query: { limit } }).json()
+  body = body.data.children
+  let res = []
+  for (let i = 0; i < body.length; i++) {
+    let item = body[i].data
+    if (['reddit.com', 'i.redd.it', 'gfycat.com', 'i.imgur.com'].includes(item.domain)) {
+      if (item.secure_media) {
+        if (item.domain === 'gfycat.com') item = item.url + '.gif'
+        else item = item.secure_media.oembed.thumbnail_url
+      } else if (item.media_metadata) {
+        let data = Object.values(item.media_metadata).find(x => x.status === 'valid')
+        item = 'https://i.redd.it/' + data.id + '.' + data.m.split('/').pop()
+      } else {
+        item = item.url
+      }
+      res.push(item)
+    }
+  }
+  return res
+}
