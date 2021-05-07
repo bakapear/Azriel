2303481b6e4a079acd1c0f3b4cb0109a019f2d56
bakapear <proxybomb@gmail.com>
2021-05-07 15:35:56 +0200

    use process.env if no .env file is present

--- src/handler/env.js
+++ src/handler/env.js
@@ -3,7 +3,8 @@ let fs = require('fs')
 
 module.exports = {
   loadEnv (path) {
-    let file = fs.readFileSync(Array.isArray(path) ? ph.join(...path) : path, 'utf-8')
+    if (!fs.existsSync(path)) return process.env
+    let file = fs.readFileSync(path, 'utf-8')
     let lines = file.split(/\r?\n/)
     return lines.reduce((acc, item) => {
       let parts = item.split('=')
