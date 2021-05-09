70daf2f652f4f11ebb96b5778aded69f2e95606a
bakapear <proxybomb@gmail.com>
2021-05-09 21:16:30 +0200

    add konachan

--- src/cmds/booru.js
+++ src/cmds/booru.js
@@ -6,6 +6,7 @@ module.exports = [
     name: 'gelbooru',
     aliases: ['gel'],
     description: 'Get a naughty image from Gelbooru',
+    usage: '(query)',
     async exec (msg, cmd) {
       let args = cmd.args.map(x => {
         if (x === 's' || x === 'safe') x = 'rating:safe'
@@ -19,15 +20,36 @@ module.exports = [
       let item = await getGelbooruImage(args)
       if (!item) return msg.channel.send('Nothing found!')
 
-      if (item.file_url.match(/\.(gif|webm|mp4)$/)) {
-        return msg.channel.send(item.file_url)
-      }
+      if (item.file_url.match(/\.(gif|webm|mp4)$/)) return msg.channel.send(item.file_url)
 
       let sample = item.file_url.replace('images', 'samples').replace('.png', '.jpg')
       let index = sample.lastIndexOf('/')
       sample = sample.substr(0, index + 1) + 'sample_' + sample.substr(index + 1)
 
       return util.sendImage(msg, item.file_url, sample)
+    }
+  },
+  {
+    name: 'konachan',
+    aliases: ['kona'],
+    description: 'Get a cute image from Konachan',
+    usage: '(query)',
+    async exec (msg, cmd) {
+      let args = cmd.args.map(x => {
+        if (x === 's' || x === 'safe') x = 'rating:safe'
+        if (x === 'q' || x === 'questionable') x = 'rating:questionable'
+        if (x === 'e' || x === 'explicit') x = 'rating:explicit'
+        if (x === 'r' || x === 'random') x = 'order:random'
+        return x
+      })
+      if (!args.length || cmd.random) args.push('order:random')
+
+      let item = await getKonachanImage(args)
+      if (!item) return msg.channel.send('Nothing found!')
+
+      if (item.file_url.match(/\.(gif|webm|mp4)$/)) return msg.channel.send(item.file_url)
+
+      return util.sendImage(msg, item.file_url, item.sample_url)
     }
   },
   {
@@ -66,5 +88,18 @@ async function getGelbooruImage (args) {
       }
     }).json()
     return res[0]
+  } catch (e) { return null }
+}
+
+async function getKonachanImage (args) {
+  try {
+    let res = await dp('https://konachan.net/post.json', {
+      query: {
+        tags: ['rating:safe', ...args].join(' '),
+        limit: 50
+      }
+    }).json()
+    // only up to 6 tags so we have to filter blacklist
+    return res.filter(x => BLACKLIST.every(y => x.tags.indexOf(y) < 0) && x.rating === 's')[0]
   } catch (e) { return null }
 }
