9d22bdc78a5b261ea32dcbdbf9d4b308cee0f289
bakapear <proxybomb@gmail.com>
2021-05-09 20:47:31 +0200

    fix velvet again

--- src/cmds/velvet.js
+++ src/cmds/velvet.js
@@ -8,7 +8,7 @@ module.exports = {
   aliases: ['redvelvet', 'rv', 'irene', 'seulgi', 'wendy', 'joy', 'yeri'],
   description: 'Get an exclusive super cute Red Velvet picture',
   async exec (msg, cmd) {
-    let member = (cmd.name === this.name || this.aliases.slice(0, 2).includes(cmd.name)) ? util.randomItem(this.aliases) : cmd.name
+    let member = (cmd.name === this.name || this.aliases.slice(0, 2).includes(cmd.name)) ? util.randomItem(this.aliases.slice(2)) : cmd.name
 
     let data = util.readRandomLine(ph.join(path, member + '.dat')).split('|')
 
