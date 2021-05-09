5ac785c33ef727ba3f01361d113550dd3714e12c
bakapear <proxybomb@gmail.com>
2021-05-09 20:43:50 +0200

    add more aliases

--- src/cmds/velvet.js
+++ src/cmds/velvet.js
@@ -5,10 +5,10 @@ let path = ph.join(__dirname, '..', 'data', 'velvet')
 
 module.exports = {
   name: 'velvet',
-  aliases: ['irene', 'seulgi', 'wendy', 'joy', 'yeri'],
+  aliases: ['redvelvet', 'rv', 'irene', 'seulgi', 'wendy', 'joy', 'yeri'],
   description: 'Get an exclusive super cute Red Velvet picture',
   async exec (msg, cmd) {
-    let member = (cmd.name === this.name) ? util.randomItem(this.aliases) : cmd.name
+    let member = (cmd.name === this.name || this.aliases.slice(0, 2).includes(cmd.name)) ? util.randomItem(this.aliases) : cmd.name
 
     let data = util.readRandomLine(ph.join(path, member + '.dat')).split('|')
 
