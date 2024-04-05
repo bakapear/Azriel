5cdda0cecba306def21cff760ea358cd531fcc9e
bakapear <proxybomb@gmail.com>
2024-04-05 18:58:33 +0200

    fix img command

--- src/cmds/image.js
+++ src/cmds/image.js
@@ -17,48 +17,38 @@ module.exports = {
 }
 
 async function searchGoogleImages (query) {
-  let body = await dp('https://www.google.com/search', {
+  let body = await dp('https://www.google.com/search?asearch=arc&hl=en&safe=strict&async=_fmt%3Apc&q=' + query, {
     headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' },
     query: {
-      source: 'lnms',
-      tbm: 'isch',
-      sa: 'X',
+      asearch: 'arc',
       hl: 'en',
       safe: 'off',
+      async: '_fmt:pc',
       q: query
     }
   }).text()
-  let result = []
-  let start = body.indexOf('data:', body.indexOf("ds:1',")) + 5
-  let end = body.indexOf(', sideChannel: {', start)
-  let json = null
-  let alt = false
-  try {
-    json = JSON.parse(body.substring(start, end))[31]
-    json = json[json.length - 1][12][2]
-  } catch (e) {
-    try {
-      alt = true
-      json = JSON.parse(body.substring(start, end))[56][1][0]
-      json = json[json.length - 1][1][0]
-    } catch (e) { return [] }
-  }
-  for (let i = 0; i < json.length; i++) {
-    let data = alt ? json[i][0][0]['444383007'][1] : json[i][1]
-    if (!data) continue
-    let item = {
-      o: {
-        u: data[3][0],
-        w: data[3][2],
-        h: data[3][1]
-      },
-      t: {
-        u: data[2][0],
-        w: data[2][2],
-        h: data[2][1]
-      }
+
+  body = JSON.parse(body.slice(body.indexOf(';[[[') + 1))[0]
+
+  let items = []
+
+  for (let item of body) {
+    item = JSON.parse(item[1])
+    if (item[0] === 1 && Array.isArray(item[1])) {
+      items.push({
+        o: {
+          u: item[1][3][0],
+          w: item[1][3][1],
+          h: item[1][3][2]
+        },
+        t: {
+          u: item[1][2][0],
+          w: item[1][2][1],
+          h: item[1][2][2]
+        }
+      })
     }
-    result.push(item)
   }
-  return result
+
+  return items
 }
